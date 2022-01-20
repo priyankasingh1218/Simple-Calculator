b325a9469c7e527a92cf185f82fade9a26ec8eae
Priyanka Singh <88391140+priyankasingh1218@users.noreply.github.com>
2022-01-20 16:48:36 +0530

    Add files via upload
    A calculator is a device that performs arithmetic operations on numbers.This is a simple calculator which does addition,subtraction and multiplication only.

---
+++ calculator(if else).cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int op,x,y;
+	cout<<"press 1 for addition,2 for subtraction,3 for multiplication \n";
+	cin>>op;
+	cout<<"enter two numbers \n";
+	cin>>x>>y;
+	if(op==1)
+	{
+		cout<<"sum is "<<x+y;
+	}
+	else if(op==2)
+	{
+		cout<<"diffrence is "<<x-y;
+	}
+	else if(op==3)
+	{
+		cout<<"product is "<<x*y;
+	}
+	else
+	{
+		cout<<"enter a valid choice";
+	}
+	return 0;
+}
